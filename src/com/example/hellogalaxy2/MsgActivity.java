c91f94ed71c68f8e2d77a5a17590f4d72c6a1cdd
student <student@PC111S36.fgakusei.local>
2014-05-28 11:07:04 +0900

    本日の作業保存

---
+++ src/com/example/hellogalaxy2/MsgActivity.java
@@ -0,0 +1,23 @@
+package com.example.hellogalaxy2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MsgActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.msg_activity);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
